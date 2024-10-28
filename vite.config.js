199bf34b00176faded839ccdeeb11117478fa0a9
unknown <fattmma117@gmail.com>
2024-10-28 11:16:59 +0300

    change value base in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sda-onsite-3-react-real-estate-website/',
+  base: './',
   plugins: [react()],
 })
